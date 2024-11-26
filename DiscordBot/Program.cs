929ae98e8f6a4dd3d222cb0d1750a2e6d128eb0c
Ruben Mateus <rubenmateus.info@gmail.com>
2024-11-26 18:02:15 +0000

    Added User Secrets | Made bot connect online

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace DiscordBot
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+
+namespace DiscordBot
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static DiscordSocketClient _client;
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            //  You can assign your bot token to a string, and pass that in to connect.
+            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+            var token = builder.GetSection("BotToken").Value;
+
+            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+            // var token = File.ReadAllText("token.txt");
+            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
         }
+
+        private static Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
     }
 }
